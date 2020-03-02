ebd713aff557f6779d4f4f650916964b97c8dbcc
Richard Paquin-Morel <rap168@the-home-mac.local>
2020-03-02 09:20:52 -0600

    fixed issue with independent variables--i.e., which proportion do I want

--- aefp_poster/aefp_poster.md
+++ aefp_poster/aefp_poster.md
@@ -7,12 +7,11 @@ affiliation:
   - num: 1
     address: Learning Research and Development Center, University of Pittsburgh
 column_numbers: 3
-logoleft_name: /Users/richardmorel/Documents/GitHub/new_project/aefp_poster/RGB_Shield_Stacked_Education_3color.png 
+logoleft_name: RGB_Shield_Stacked_Education_3color.png 
 output: 
   posterdown::posterdown_html:
     self_contained: false
     keep_md: true
-    css: /Users/richardmorel/Documents/GitHub/new_project/aefp_poster/pitt_colors.css
 bibliography: packages.bib
 ---
 
@@ -20,14 +19,15 @@ bibliography: packages.bib
 
 # Overview
 
-While school choice policies are now ubiqitous are often presented as a strategy for addressing persistent racial disparities education, there is concern that such policies may maintain or increases such disparities due to resource hoarding by advantaged families or through increased racial segregation. Using a group threat/status anxiety framework, I hypothesize that increased racial diversification may induce white families to enroll their children in charter schools. I distinguish between proximal status threat, where racial diversification occurs in neighboring school districts, and direct status threat, where racial diversification occurs within local traditional public schools. I find evidence that both proximal and direct threat are associated with both increased foundings of charter schools and with white enrollment in charter schools. [IMPLICATIONS]
+While school choice policies are now ubiqitous are often presented as a strategy for addressing persistent racial disparities education, there is concern that such policies may maintain or increases such disparities due to resource hoarding by advantaged families or through increased racial segregation. Using a group threat/status anxiety framework, I hypothesize that increased racial diversification may induce white families to enroll their children in charter schools. I distinguish between proximal status threat, where racial diversification occurs in neighboring school districts, and direct status threat, where racial diversification occurs within local traditional public schools. I find evidence that both proximal and direct threat are associated with both increased foundings of charter schools and with white enrollment in charter schools. 
 
+[SOME IMPLICATIONS]
 
 # Data Methods
 
-**Data**
+**Data**. Panel of California school district data from 2000-2015. 
 
-Panel of California school district data from 2000-2015. Sources:
+Sources:
 
 - District enrollment and finance data: The Common Core of Data (accessed via the Urban Institutes API) 
 - District poverty data: Small Area Income and Poverty Estimates (accessed via the Urban Institutes API) 
@@ -45,116 +45,25 @@ $i$ indexes districts, $j$ indexes years, and $k$ indexes districts that neighbo
 
 **Analytic model: Fixed Effects Regression**
 
-$$ Y_{ij} = \beta_1ProxThreat_{ij} + \beta_2DirThreat_{ij} + X_{it}\beta + \alpha_i + \gamma_j + \epsilon_{ij} $$
-
+$$ Y_{ij} = \beta_1ProxThreat_{ij} + \beta_2DirThreat_{ij} + X_{ij}\beta + \alpha_i + \gamma_j + \epsilon_{ij} $$
+<br>  
+<br>  
+<br>  
+<br>  
 
 # Results
 
 Usually you want to have a nice table displaying some important results that you have calculated. In `posterdown` this is as easy as using the `kable` table formatting you are probably use to as per typical R Markdown formatting.
 
-You can reference tables like so: Table \@ref(tab:mytable). Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat augue at velit tincidunt semper. Donec elementum porta posuere. Nullam interdum, odio at tincidunt feugiat, turpis nisi blandit eros, eu posuere risus felis non quam. Nam eget lorem odio. Duis et aliquet orci. Phasellus nec viverra est.
-
-<table>
-<caption>(\#tab:mytable)Table caption.</caption>
- <thead>
-  <tr>
-   <th style="text-align:center;"> Sepal.Length </th>
-   <th style="text-align:center;"> Sepal.Width </th>
-   <th style="text-align:center;"> Petal.Length </th>
-   <th style="text-align:center;"> Petal.Width </th>
-  </tr>
- </thead>
-<tbody>
-  <tr>
-   <td style="text-align:center;"> 5.1 </td>
-   <td style="text-align:center;"> 3.5 </td>
-   <td style="text-align:center;"> 1.4 </td>
-   <td style="text-align:center;"> 0.2 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 4.9 </td>
-   <td style="text-align:center;"> 3.0 </td>
-   <td style="text-align:center;"> 1.4 </td>
-   <td style="text-align:center;"> 0.2 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 4.7 </td>
-   <td style="text-align:center;"> 3.2 </td>
-   <td style="text-align:center;"> 1.3 </td>
-   <td style="text-align:center;"> 0.2 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 4.6 </td>
-   <td style="text-align:center;"> 3.1 </td>
-   <td style="text-align:center;"> 1.5 </td>
-   <td style="text-align:center;"> 0.2 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 5.0 </td>
-   <td style="text-align:center;"> 3.6 </td>
-   <td style="text-align:center;"> 1.4 </td>
-   <td style="text-align:center;"> 0.2 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 5.4 </td>
-   <td style="text-align:center;"> 3.9 </td>
-   <td style="text-align:center;"> 1.7 </td>
-   <td style="text-align:center;"> 0.4 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 4.6 </td>
-   <td style="text-align:center;"> 3.4 </td>
-   <td style="text-align:center;"> 1.4 </td>
-   <td style="text-align:center;"> 0.3 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 5.0 </td>
-   <td style="text-align:center;"> 3.4 </td>
-   <td style="text-align:center;"> 1.5 </td>
-   <td style="text-align:center;"> 0.2 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 4.4 </td>
-   <td style="text-align:center;"> 2.9 </td>
-   <td style="text-align:center;"> 1.4 </td>
-   <td style="text-align:center;"> 0.2 </td>
-  </tr>
-  <tr>
-   <td style="text-align:center;"> 4.9 </td>
-   <td style="text-align:center;"> 3.1 </td>
-   <td style="text-align:center;"> 1.5 </td>
-   <td style="text-align:center;"> 0.1 </td>
-  </tr>
-</tbody>
-</table>
-
-Or with figures: Figure \@ref(fig:standard-plot), or Figure \@ref(fig:morefigs).
-
-<div class="figure" style="text-align: center">
-<img src="aefp_poster_files/figure-html/standard-plot-1.png" alt="Great figure!" width="80%" />
-<p class="caption">(\#fig:standard-plot)Great figure!</p>
-</div>
+<img src="aefp_poster_files/figure-html/standard-plot-1.png" width="90%" style="display: block; margin: auto;" />
 
 
-```r
-data <- iris
-
-plot(x = data$Sepal.Length, 
-     y = data$Sepal.Width, 
-     col = data$Species,
-     pch = 19, 
-     xlab = "Sepal Length (cm)",
-     ylab = "Sepal Width (cm)")
-```
-
-<div class="figure">
-<img src="aefp_poster_files/figure-html/morefigs-1.png" alt="Amazing, right?!" width="80%" />
-<p class="caption">(\#fig:morefigs)Amazing, right?!</p>
-</div>
+<img src="aefp_poster_files/figure-html/morefigs-1.png" width="90%" style="display: block; margin: auto;" />
 
 # Next Steps
 
-Aliquam sed faucibus risus, quis efficitur erat. Vestibulum semper mauris quis tempus eleifend. Aliquam sagittis dictum ipsum, quis viverra ligula eleifend ut. Curabitur sagittis vitae arcu eget faucibus. In non elementum felis. Duis et aliquam nunc. Nunc pulvinar sapien nunc, vel pretium nisi efficitur in. Fusce fringilla maximus leo et maximus. Fusce at ligula laoreet, iaculis mi at, auctor odio. Praesent sed elementum justo. Aenean consectetur risus rhoncus tincidunt efficitur. Praesent dictum mauris at diam maximus maximus [@R-posterdown].
+<img src="aefp_poster_files/figure-html/map1-1.png" width="100%" style="display: block; margin: auto auto auto 0;" /><img src="aefp_poster_files/figure-html/map1-2.png" width="100%" style="display: block; margin: auto auto auto 0;" />
+
 
 # Conclusion
 
--- ca_charter_analysis.R
+++ ca_charter_analysis.R
@@ -3,6 +3,7 @@ library(educationdata)
 library(tidycensus)
 library(lfe)
 library(sf)
+library(huxtable)
 
 load("charter_analysis.RData")
 load("district_shapefiles.RData")
@@ -10,7 +11,35 @@ load("district_shapefiles.RData")
 ## California districts
 d_dat <- 
   d_dat %>% 
+  filter(fips == 6) %>% 
+  select(-contains("est_population"))
+
+## California schools
+s_dat <-
+  s_dat %>% 
   filter(fips == 6)
+
+# CA poverty data
+d_pov <-
+  get_education_data(
+    level = "school-districts",
+    source = "saipe",
+    filters = list(
+      year = 2000:2015,
+      fips = 6
+    )
+  ) %>%
+  mutate(leaid = as.numeric(leaid)) %>%
+  as_tibble() %>%
+  na_if(-1) %>%
+  na_if(-2) %>%
+  na_if(-3) %>%
+  na_if(-9) %>%
+  arrange(leaid, year) %>% 
+  select(leaid, year, contains("est_population"))
+
+d_dat <- 
+  left_join(d_dat, d_pov)
 
 ## Determine threat ----
 shapefiles <- 
@@ -152,10 +181,6 @@ district_borders <-
 rm(district_borders_even)
 rm(district_borders_odd)
 
-rm(shapefiles)
-
-save.image("threat_analysis_04_06_08_26_41.RData")
-
 ## 6) join district and threat data ----
 d_dat <-
   d_dat %>%
@@ -167,31 +192,30 @@ charter_dat <-
   s_dat %>%
   group_by(leaid, year) %>%
   summarize(
-    n_charter = sum(charter, na.rm = T)
+    n_charter = sum(charter, na.rm = T),
+    n_total = n()
   ) %>%
-  ungroup() %>%
+  ungroup() %>% 
+  mutate(prop_charter = n_charter / n_total) %>% 
   arrange(leaid, year)
 
 charter_demo <-
   s_dat %>%
-  group_by(leaid, year, charter) %>%
-  summarize(
-    prop_white = sum(white, na.rm = T) / sum(total, na.rm = T),
-    prop_black = sum(black, na.rm = T) / sum(total, na.rm = T),
-    prop_asian = sum(asian, na.rm = T) / sum(total, na.rm = T),
-    prop_hispanic = sum(hispanic, na.rm = T) / sum(total, na.rm = T),
-    prop_other = sum(other, na.rm = T) / sum(total, na.rm = T)
-  ) %>%
-  ungroup() %>% 
+  group_by(leaid, year, charter) %>% 
+  summarize_at(vars(hispanic:other), list(n = ~ sum(., na.rm = TRUE))) %>% 
+  ungroup() %>%
   complete(charter, nesting(leaid, year), 
-           fill = list(prop_white = 0, prop_black = 0, 
-                       prop_asian = 0, prop_hispanic = 0, 
-                       prop_other = 0)) %>% 
+           fill = list(hispanic_n = 0,
+                       white_n = 0, black_n = 0, 
+                       native_am_n = 0, asian_n = 0, 
+                       total_n = 0, pacific_n = 0, 
+                       two_or_more_n = 0, other_n = 0)) %>% 
   arrange(leaid, year, charter) %>% 
-  mutate_at(
-    vars(starts_with("prop")),
-    ~ round(., 2)
-  ) %>%
+  group_by(leaid, year) %>% 
+  mutate_at(vars(hispanic_n:other_n), list(within_prop = ~ round(./sum(., na.rm = TRUE), 2))) %>% 
+  mutate_at(vars(hispanic_n:other_n), list(overall_prop = ~ round(./sum(total_n, na.rm = TRUE), 2))) %>% 
+  ungroup() %>%
+  mutate_at(vars(hispanic_n:other_n), list(btw_prop = ~ round(./total_n, 2))) %>% 
   mutate(
     charter =
       ifelse(
@@ -200,38 +224,29 @@ charter_demo <-
         "traditional"
       )
   ) %>%
-  ungroup() %>%
-  pivot_wider(names_from = charter, values_from = prop_white:last_col())
+  pivot_wider(names_from = charter, values_from = hispanic_n:last_col())
 
 charter_dat <-
   charter_dat %>%
   left_join(charter_demo)
 
-# merge with district data and calculate proportion of charters
+# merge with district data
 charter_dat <-
   charter_dat %>%
-  left_join(
-    d_dat
-  ) %>%
-  mutate(
-    prop_charter = n_charter / number_of_schools
-  ) %>%
-  group_by(leaid) %>%
-  mutate(
-    change_charter = n_charter - lag(n_charter)
-  ) %>%
-  ungroup()
+  left_join(d_dat)
 
 charter_dat <-
   charter_dat %>%
-  filter(number_of_schools != 1)
+  filter(number_of_schools != 1) %>% 
+  mutate(nonwhite_n_traditional = hispanic_n_traditional + black_n_traditional + native_am_n_traditional + asian_n_traditional + pacific_n_traditional + two_or_more_n_traditional + other_n_traditional) %>% 
+  mutate(nonwhite_n_prop_traditional = nonwhite_n_traditional / total_n_traditional)
 
 # keep the districts in non-urban areas & majority white districts & affluent districts
 charter_dat <-
   charter_dat %>%
   mutate(
-    prop_local = rev_local_total / rev_total,
-    prop_state = rev_state_total / rev_total,
+    prop_local_rev = rev_local_total / rev_total,
+    prop_state_rev = rev_state_total / rev_total,
     prop_local_prop_tax = rev_local_prop_tax / rev_total,
     per_pupil_spending = exp_total / total
   )
@@ -240,6 +255,12 @@ charter_dat_non_urban <-
   charter_dat %>%
   group_by(leaid) %>%
   filter(!urban_centric_locale %in% c(1, 2, 11, 12)) %>%
+  ungroup()
+
+charter_dat_urban <-
+  charter_dat %>%
+  group_by(leaid) %>%
+  filter(urban_centric_locale %in% c(1, 2, 11, 12)) %>%
   ungroup()
 
 charter_white_quant <-
@@ -276,145 +297,211 @@ charter_dat_maj_white <-
 charter_affluent <-
   charter_dat %>%
   group_by(year) %>%
-  mutate(pov_25th = quantile(est_population_5_17_poverty_pct, 0.25, na.rm = T)) %>%
+  mutate(pov_quant = ntile(est_population_5_17_poverty_pct, 5)) %>%
   group_by(leaid) %>%
-  filter(sum(est_population_5_17_poverty_pct <= pov_25th, na.rm = T) > (n() / 2)) %>%
+  filter(sum(pov_quant == 1, na.rm = T) > (n() / 2)) %>%
   ungroup()
 
+rm(list = ls()[str_detect(ls(), "d_")][-2])
+rm(list = ls()[str_detect(ls(), "s_")])
+
+save.image("ca_threat_analysis.RData")
 
 
-
-summary(
+# Models 1: Share of charters in a district ----
+m1_base <- 
   felm(
     prop_charter ~
       threat_overall +
-      prop_white_traditional +
-      prop_black_traditional +
-      prop_hispanic_traditional +
-      log(enrollment) +
-      lag(prop_local) +
-      lag(prop_local_prop_tax) +
-      lag(per_pupil_spending) | leaid + year | 0 | leaid,
+      black_n_btw_prop_traditional +
+      hispanic_n_btw_prop_traditional | 0 | 0 | leaid,
     data = charter_dat
   )
-)
 
-
-summary(
+m1_base_fes <- 
   felm(
     prop_charter ~
-    lag(prop_charter) +
+      threat_overall +
+      black_n_btw_prop_traditional +
+      hispanic_n_btw_prop_traditional | leaid + year | 0 | leaid,
+    data = charter_dat
+  )
+
+m1_pooled <- 
+  felm(
+    prop_charter ~
+      threat_overall +
+      black_n_btw_prop_traditional +
+      hispanic_n_btw_prop_traditional +
+      white_n_btw_prop_traditional +
+      log(enrollment) +
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | 0 | 0 | leaid,
+    data = charter_dat
+  )
+
+m1_controls <- 
+  felm(
+    prop_charter ~
+      threat_overall +
+      black_n_btw_prop_traditional +
+      hispanic_n_btw_prop_traditional +
+      white_n_btw_prop_traditional +
+      log(enrollment) +
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | leaid + year | 0 | leaid,
+    data = charter_dat
+  )
+
+coefs_names <- c("Proximal Threat", "%Black in TPS", "%Hispanic in TPS")
+huxreg(list(m1_base, m1_base_fes ,m1_pooled, m1_controls), 
+       coefs = c("Proximal threat" = "threat_overall",
+                 "%Black in TPS" = "black_n_btw_prop_traditional", 
+                 "%Hispanic in TPS" = "hispanic_n_btw_prop_traditional"),
+       statistics = c('N' = 'nobs', 'R-squared' = 'r.squared'),
+       note = "{stars}. \nStandard errors clustered at the district level. \nControls include the natural log of enrollment, local revenue, and per-student spending, and the share of 5-17 in poverty.") %>% 
+  insert_row(c("District Fixed Effects", "", "X", "", "X"), after = 7) %>% 
+  insert_row(c("Year Fixed Effects", "", "X", "", "X"), after = 8) %>% 
+  insert_row(c("Controls", "", "", "X", "X"), after = 9) %>% 
+  set_bottom_border(8, 2:5, 0) %>% 
+  set_bottom_border(9, 2:5, 0) %>% 
+  set_bottom_border(10, 2:5, 0)
+  
+
+# Models 2: White charter enrollment in a district ----
+m2_base <- 
+  felm(
+    white_n_within_prop_charter ~
+      threat_overall +
+      black_n_btw_prop_traditional +
+      hispanic_n_btw_prop_traditional | 0 | 0 | leaid,
+    data = charter_dat
+  )
+
+m2_base_fes <- 
+  felm(
+    white_n_within_prop_charter ~
+      threat_overall +
+      black_n_btw_prop_traditional +
+      hispanic_n_btw_prop_traditional | leaid + year | 0 | leaid,
+    data = charter_dat
+  )
+
+m2_pooled <- 
+  felm(
+    white_n_within_prop_charter ~
+      threat_overall +
+      black_n_btw_prop_traditional +
+      hispanic_n_btw_prop_traditional +
+      log(enrollment) +
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | 0 | 0 | leaid,
+    data = charter_dat
+  )
+
+m2_controls <- 
+  felm(
+    white_n_within_prop_charter ~
+      threat_overall +
+      black_n_btw_prop_traditional +
+      hispanic_n_btw_prop_traditional +
+      log(enrollment) +
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | leaid + year | 0 | leaid,
+    data = charter_dat
+  )
+
+huxreg(list("pooled" = m2_base, "fes" = m2_base_fes, "pooled" = m2_pooled, "fes" = m2_controls))
+
+# Models 3: Proximal threat ----
+m3_overall_prox <- 
+  felm(
+    prop_white_enroll_charter ~
+      threat_overall +
+      prop_white_enroll_traditional +
+      log(enrollment) +
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | leaid + year | 0 | leaid,
+    data = charter_dat
+  )
+
+m3_prox <- 
+  felm(
+    prop_white_enroll_charter ~
       threat_black +
-      threat_hispanic +
-      seg +
-      prop_white_traditional +
-      prop_black_traditional +
-      prop_hispanic_traditional +
+      threat_hispanic + 
+      prop_white_enroll_traditional +
       log(enrollment) +
-      lag(prop_local) +
-      lag(prop_local_prop_tax) +
-      lag(per_pupil_spending) | leaid + year | 0 | leaid,
-    data = charter_dat_maj_white
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | leaid + year | 0 | leaid,
+    data = charter_dat
   )
-)
 
-summary(
+huxreg(list(m3_overall_prox, m3_prox))
+
+# Models 4: Direct threat ----
+m4_overall_direct <- 
   felm(
-    prop_white_charter ~
-    threat_overall +
-      seg +
-      prop_white_traditional +
-      prop_black_traditional +
-      prop_hispanic_traditional +
+    prop_white_enroll_charter ~
+      prop_nonwhite_enroll_traditional +
+      #prop_white_enroll_traditional +
       log(enrollment) +
-      lag(prop_local) +
-      lag(prop_local_prop_tax) +
-      lag(per_pupil_spending) | leaid + year | 0 | leaid,
-    data = charter_dat_maj_white
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | leaid + year | 0 | leaid,
+    data = charter_dat
   )
-)
 
-summary(
+m4_direct <- 
   felm(
-    prop_white_charter ~
-    threat_black +
-      threat_hispanic +
-      seg +
-      prop_white_traditional +
-      prop_black_traditional +
-      prop_hispanic_traditional +
+    prop_white_enroll_charter ~
+      prop_black_enroll_traditional +
+      prop_hispanic_enroll_traditional +
+      prop_white_enroll_traditional +
       log(enrollment) +
-      lag(prop_local) +
-      lag(prop_local_prop_tax) +
-      lag(per_pupil_spending) | leaid + year | 0 | leaid,
-    data = charter_dat_maj_white
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | leaid + year | 0 | leaid,
+    data = charter_dat
   )
-)
 
+huxreg(list(m4_overall_direct, m4_direct))
 
-summary(
+# Models 5: Urban v Non-urban ----
+m5_non <- 
   felm(
-    prop_black_charter ~
-    threat_overall +
-      seg +
-      prop_white_traditional +
-      prop_black_traditional +
-      prop_hispanic_traditional +
+    prop_white_enroll_charter ~
+      threat_overall +
+      prop_black_enroll_traditional +
+      prop_hispanic_enroll_traditional +
+      prop_white_enroll_traditional +
       log(enrollment) +
-      lag(prop_local) +
-      lag(prop_local_prop_tax) +
-      lag(per_pupil_spending) | leaid + year | 0 | leaid,
-    data = charter_dat_maj_white
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | leaid + year | 0 | leaid,
+    data = charter_dat_non_urban
   )
-)
 
-summary(
+m5_urban <- 
   felm(
-    prop_hispanic_charter ~
-    threat_overall +
-      seg +
-      prop_white_traditional +
-      prop_black_traditional +
-      prop_hispanic_traditional +
+    prop_white_enroll_charter ~
+      threat_overall +
+      prop_black_enroll_traditional +
+      prop_hispanic_enroll_traditional +
+      prop_white_enroll_traditional +
       log(enrollment) +
-      lag(prop_local) +
-      lag(prop_local_prop_tax) +
-      lag(per_pupil_spending) | leaid + year | 0 | leaid,
-    data = charter_dat_maj_white
+      log(rev_local_total) +
+      log(per_pupil_spending) +
+      est_population_5_17_poverty_pct | leaid + year | 0 | leaid,
+    data = charter_dat_urban
   )
-)
+
+huxreg(list(m5_urban, m5_non))
 
 
-summary(
-  felm(
-    prop_black_charter ~
-    threat_black +
-      threat_hispanic +
-      seg +
-      prop_white_traditional +
-      prop_black_traditional +
-      prop_hispanic_traditional +
-      log(enrollment) +
-      lag(prop_local) +
-      lag(prop_local_prop_tax) +
-      lag(per_pupil_spending) | leaid + year | 0 | leaid,
-    data = charter_dat_maj_white
-  )
-)
-
-summary(
-  felm(
-    prop_hispanic_charter ~
-    threat_black +
-      threat_hispanic +
-      seg +
-      prop_white_traditional +
-      prop_black_traditional +
-      prop_hispanic_traditional +
-      log(enrollment) +
-      lag(prop_local) +
-      lag(prop_local_prop_tax) +
-      lag(per_pupil_spending) | leaid + year | 0 | leaid,
-    data = charter_dat_maj_white
-  )
-)
